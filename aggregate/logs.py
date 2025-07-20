6611a6e78404724efb938acd87a6c02d48c6e958
Valentin Teutschbein <mail@valteu.com>
2025-07-21 00:56:13 +0200

    fix doubleclick bug, mouse click debouncing bug

--- aggregate/logs.py
+++ aggregate/logs.py
@@ -21,7 +21,6 @@ def extract_key(details: dict, pretty=False) -> str:
     return key
 
 
-# TODO: Refactor
 def calculate_breaks(events: list, percentile: int):
     timestamps = []
     per_event_ts = {}
@@ -34,7 +33,7 @@ def calculate_breaks(events: list, percentile: int):
 
     breaks, durations, thresholds = {}, {}, {}
     for evt, ts_list in per_event_ts.items():
-        if evt == "mouse_down":
+        if evt in ["mouse_down", "poll"]:
             continue
         if len(ts_list) < 2:
             breaks[evt], durations[evt], thresholds[evt] = [], [], 0
@@ -46,65 +45,43 @@ def calculate_breaks(events: list, percentile: int):
         idx = np.where(deltas >= thresh)[0]
         breaks[evt] = [(ts_list[i], ts_list[i + 1]) for i in idx]
         durations[evt] = deltas.tolist()
-
-    mouse_down_events = [e for e in events if e.event_type == "mouse_down"]
-
-    processed_mouse_events = []
-    last_pos, last_time = None, None
-
-    def remove_lst_double_click(ts, pos, events):
-        for i in range(len(events) - 1, -1, -1):
-            evt_ts = datetime.strptime(events[i].timestamp, "%Y-%m-%d_%H-%M-%S-%f")
-            if evt_ts == ts and events[i].cursor_pos == pos:
-                del events[i]
-        return events
-
-    for evt in mouse_down_events:
-        ts = datetime.strptime(evt.timestamp, "%Y-%m-%d_%H-%M-%S-%f")
-        button = evt.details.get('button', 'unknown')
-
-        if "left" not in button.lower():
-            processed_mouse_events.append(evt)
-            continue
-
-        pos = evt.cursor_pos
-        monitor_dim = [evt.monitor.get("width", 1920), evt.monitor.get("height", 1080)]
-
-        new_evt = evt.copy()
-
-        if last_pos is None or last_time is None:
-            last_pos, last_time = pos, ts
-            processed_mouse_events.append(new_evt)
-            continue
-
-        time_diff = (ts - last_time).total_seconds() * 1000
-
-        if (time_diff <= MOUSE_DOWN_DEBOUNCE and not None in last_pos + pos and
-            abs(pos[0] - last_pos[0]) <= MOUSE_DOWN_POS_OFFSET * monitor_dim[0] and
-                abs(pos[1] - last_pos[1]) <= MOUSE_DOWN_POS_OFFSET * monitor_dim[1]):
-
-            new_evt.details['double_click'] = True
-            processed_mouse_events.append(new_evt)
-            processed_mouse_events = remove_lst_double_click(last_time, last_pos, processed_mouse_events)
-        else:
-            last_pos, last_time = pos, ts
-            processed_mouse_events.append(new_evt)
-
-    mouse_down_timestamps = []
-    for evt in processed_mouse_events:
-        ts = datetime.strptime(evt.timestamp, "%Y-%m-%d_%H-%M-%S-%f")
-        mouse_down_timestamps.append(ts)
-
-    mouse_down_breaks = []
-    if len(mouse_down_timestamps) > 1:
-        for i in range(len(mouse_down_timestamps) - 1):
-            mouse_down_breaks.append((mouse_down_timestamps[i], mouse_down_timestamps[i + 1]))
-
-    breaks["mouse_down"] = mouse_down_breaks
-    durations["mouse_down"] = []
-    thresholds["mouse_down"] = 0
-
     return timestamps, breaks, durations, thresholds
+
+
+def debounce_double_clicks(events):
+    logs = RawLogEvents()
+    processed_events = []
+    for i, event in enumerate(events):
+        if event.event_type != "mouse_down" or "left" not in event.details.get('button', '').lower():
+            processed_events.append(event)
+            continue
+        ts = datetime.strptime(event.timestamp, "%Y-%m-%d_%H-%M-%S-%f")
+        pos = event.cursor_pos
+        monitor_dim = [event.monitor.get("width", 1920), event.monitor.get("height", 1080)]
+
+        for new_event in events[i + 1:]:
+            if new_event.event_type != "mouse_down":
+                continue
+            new_ts = datetime.strptime(new_event.timestamp, "%Y-%m-%d_%H-%M-%S-%f")
+            if (new_ts - ts).total_seconds() * 1000 > MOUSE_DOWN_DEBOUNCE:
+                processed_events.append(event)
+                break
+            new_pos = new_event.cursor_pos
+
+            if abs(new_pos[0] - pos[0]) <= MOUSE_DOWN_POS_OFFSET * monitor_dim[0] and \
+               abs(new_pos[1] - pos[1]) <= MOUSE_DOWN_POS_OFFSET * monitor_dim[1]:
+                new_event.details['double_click'] = True
+                processed_events.append(new_event)
+
+                for j in range(i + 1, len(events)):
+                    if events[j].event_type == "mouse_up" and events[j].details.get('button', '').lower() == 'left':
+                        del events[j]
+                break
+            else:
+                processed_events.append(event)
+                break
+    logs.events = processed_events
+    return logs
 
 
 def sort_and_fill_edges(breaks, gap_threshold=60):
@@ -130,33 +107,57 @@ def sort_and_fill_edges(breaks, gap_threshold=60):
     return result
 
 
-def _debounce_press_event(events, debounce_event, max_duration):
+def _debounce_press_event(events, debounce_event, max_duration, offset):
     def get_event_detail(event):
-        if event.event_type == 'mouse_down':
+        if event.event_type in ['mouse_down', 'mouse_up']:
             return event.details.get('button')
-        elif event.event_type == 'keyboard_press':
+        elif event.event_type in ['keyboard_press', 'keyboard_release']:
             return extract_key(event.details)
 
     cut_idx = None
     for lookahead in events:
         switched_screen = json.dumps(debounce_event.monitor) != json.dumps(lookahead.monitor)
-        if switched_screen or max_duration > lookahead.strp_timestamp - debounce_event.strp_timestamp:
+        if switched_screen or (lookahead.strp_timestamp - debounce_event.strp_timestamp) > max_duration:
             break
-        if lookahead.event_type == debounce_event.event_type and get_event_detail(lookahead) == get_event_detail(debounce_event):
-            cut_idx = events.index(lookahead)
-            break
+
+        if debounce_event.event_type == 'mouse_down':
+            if (lookahead.event_type == 'mouse_up' and
+                    get_event_detail(lookahead) == get_event_detail(debounce_event)):
+
+                pos = debounce_event.cursor_pos
+                new_pos = lookahead.cursor_pos
+                monitor_dim = [debounce_event.monitor.get("width", 1920),
+                               debounce_event.monitor.get("height", 1080)]
+
+                if (abs(new_pos[0] - pos[0]) <= MOUSE_DOWN_POS_OFFSET * monitor_dim[0] and
+                        abs(new_pos[1] - pos[1]) <= MOUSE_DOWN_POS_OFFSET * monitor_dim[1]):
+                    cut_idx = events.index(lookahead) + offset
+                    break
+
+        elif debounce_event.event_type == 'keyboard_press':
+            if (lookahead.event_type == 'keyboard_release' and
+                    get_event_detail(lookahead) == get_event_detail(debounce_event)):
+                cut_idx = events.index(lookahead) + offset
+                break
+
     return cut_idx
 
 
 def aggregate_logs(events: RawLogEvents, breaks: dict):
     events.sort()
     edges = sort_and_fill_edges(breaks)
+    events = debounce_double_clicks(events)
 
     logs = []
     start_idx = 0
+    cut_idx = 0
 
     for edge in edges:
         for rel_idx, event in enumerate(events[start_idx:], start=start_idx):
+            if event.event_type == 'mouse_down':
+                cut_idx = rel_idx
+                break
+
             ev_time = event.strp_timestamp
             next_ev_time = events[rel_idx + 1].strp_timestamp if rel_idx + 1 < len(events) else float('inf')
             switched_screen = json.dumps(event.monitor) != json.dumps(events[start_idx].monitor)
@@ -171,8 +172,8 @@ def aggregate_logs(events: RawLogEvents, breaks: dict):
             switched_screen = False
 
         last_event = events[cut_idx]
-        if last_event.event_type not in ['mouse_down', 'keyboard_press']:
-            _cut_idx = _debounce_press_event(events, last_event, MAX_RELEASE_EVENT_DURATION)
+        if last_event.event_type in ['mouse_down', 'keyboard_press']:
+            _cut_idx = _debounce_press_event(events[cut_idx + 1:], last_event, MAX_RELEASE_EVENT_DURATION, cut_idx + 1)
             cut_idx = _cut_idx if _cut_idx is not None else cut_idx
 
         logs.append(AggregatedLog.from_raw_log_events(events[start_idx:cut_idx + 1]))
@@ -192,7 +193,6 @@ def main(path, percentile=95):
     logs.sort()
     debounced_logs = RawLogEvents()
     debounced_logs.events = [log for log in logs if log.screenshot_path]
-    logs.events = [log for log in logs if log.event_type != 'poll']
     timestamps, breaks, durations, thresholds = calculate_breaks(logs, percentile)
     return aggregate_logs(debounced_logs, breaks)
 
